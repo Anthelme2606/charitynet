b16a277edfcce19bba9c4248e2179db43de26605
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-10-30 10:03:14 +0000

    committng

--- client/src/components/don-manager.js
+++ client/src/components/don-manager.js
@@ -30,60 +30,12 @@ const users = [
 ];
 
 const DonManager = () => {
-<<<<<<< HEAD:client/src/components/don-manager.js
-    const handleView = (email) => {
-        alert(`Viewing details for ${email}`);
-        return (
-          <div
-            class="modal fade"
-            id="exampleModal"
-            tabindex="-1"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">
-                    Titre du Modal
-                  </h5>
-                  <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Fermer"
-                  ></button>
-                </div>
-                <div class="modal-body">
-                  <p>
-                    Voici le contenu du modal. Vous pouvez mettre ici tout type
-                    de contenu HTML.
-                  </p>
-                </div>
-                <div class="modal-footer">
-                  <button
-                    type="button"
-                    class="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                  >
-                    Fermer
-                  </button>
-                  <button type="button" class="btn btn-primary">
-                    Enregistrer les changements
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-=======
     const [openDetail, setOpenDetail] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
 
     const handleView = (user) => {
         setSelectedUser(user);
         setOpenDetail(true);
->>>>>>> origin/anthelme-feature:src/components/don-manager.js
     };
 
     const handleDisable = (email) => {
